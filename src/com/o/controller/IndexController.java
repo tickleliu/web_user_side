5124195491617c4b58157f4f75423547689c21a3
liuminglu19870419 <liuminglu19870419@163.com>
2016-07-30 10:38:18 +0800

    简化了当前网站的功能，删除mongodb的支持，添加共调试开发的index页

--- src/com/o/controller/IndexController.java
+++ src/com/o/controller/IndexController.java
@@ -11,11 +11,11 @@ public class IndexController {
 
 	Logger logger = Logger.getLogger(IndexController.class);
 	
-	@RequestMapping(value={"/", "index"})
-	@CachePut(value="user", key="getIndexPage")
-	@Cacheable(value = "user", key="getIndexPage")
-	String getIndexPage() {
-		logger.debug("index page request");
-		return "index";
-	}
+//	@RequestMapping(value={"/", "index"})
+//	@CachePut(value="user", key="getIndexPage")
+//	@Cacheable(value = "user", key="getIndexPage")
+//	String getIndexPage() {
+//		logger.debug("index page request");
+//		return "index";
+//	}
 }
